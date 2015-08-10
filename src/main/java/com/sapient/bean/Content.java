32fc65d645f54d88e9a083f0c408030fbe69d121
Amol Amol <aamol@sapient.com>
2015-08-10 16:45:49 +0530

    first commit

---
+++ src/main/java/com/sapient/bean/Content.java
@@ -0,0 +1,48 @@
+package com.sapient.bean;
+
+import org.springframework.data.annotation.Id;
+
+public class Content {
+
+	@Id
+	private String id;
+
+	private final String contentID;
+	private final String contentData;
+
+	/**
+	 * @param contentID
+	 * @param contentData
+	 */
+	public Content(String contentID, String contentData) {
+		this.contentID = contentID;
+		this.contentData = contentData;
+	}
+
+	/**
+	 * @return the contentID
+	 */
+	public String getContentID() {
+		return contentID;
+	}
+
+	/**
+	 * @return the contentData
+	 */
+	public String getContentData() {
+		return contentData;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Content [id=" + id + ", contentID=" 
+	+ contentID + ", contentData="
+				+ contentData + "]";
+	}
+
+}

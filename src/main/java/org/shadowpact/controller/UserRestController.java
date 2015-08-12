fba86cf6f0120eda84d41a74ca4c2056bb51d4f1
raghav <rarora9@sapient.com>
2015-08-12 23:23:30 +0530

    Committing code fir user profile

--- src/main/java/org/shadowpact/controller/UserRestController.java
+++ src/main/java/org/shadowpact/controller/UserRestController.java
@@ -41,19 +41,28 @@ public class UserRestController {
 	}
 
 	@RequestMapping("/registerUser")
-	    public User registerNewUser(@RequestParam(value="name") String name, @RequestParam(value="email") String email, 
-	    		@RequestParam(value="password") String password) {
-	     System.out.println("User details:: name: " + name + " email: " + email + " password: " + password);   
-		 boolean userExists = userHelper.validateNewUser(email);
-		 User userBean = new User(name, email, password, null, null, null, null);
-		 if (!userExists) {
-			 userBean = userRepository.save(userBean);
-			 System.out.println(userBean);
-			 userBean = new User(name, email, password, configObj.getHttpResponseCodeSuccess(), null, null, null);
-	     } else {
-	    	 userBean = new User(name, email, password, configObj.getHttpResponseCodeSuccess(), null, configObj.getUserExistsMessage(), null);
-	     }
-		 return userBean;
+    public User registerNewUser(@RequestParam(value="name") String name, @RequestParam(value="email") String email, 
+    		@RequestParam(value="password") String password) {
+
+		System.out.println("User details:: name: " + name + " email: " + email + " password: " + password);   
+		boolean userExists = userHelper.validateNewUser(email);
+		User userBean = new User(name, email, password, null, null, null, null);
+		if (!userExists) {
+			userBean = userRepository.save(userBean);
+			System.out.println(userBean);
+			userBean = new User(name, email, password, configObj.getHttpResponseCodeSuccess(), null, null, null);
+	    } else {
+	    	userBean = new User(name, email, password, configObj.getHttpResponseCodeSuccess(), null, configObj.getUserExistsMessage(), null);
 	    }
+		return userBean;
+    }
+
+	@RequestMapping("/saveUserProfile")
+	public User saveUserProfile(@RequestParam(value = "email") String email, 
+			@RequestParam(value = "password") String password) {
+		System.out.println("User email value as: " + email);
+		// TODO
+		return null;
+	}
 
 }
